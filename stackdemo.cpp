c9d36c00d1dd139fde0cc2c0b2afaf36c8a75579
ecepanagiotisntua <61820984+ecepanagiotisntua@users.noreply.github.com>
2020-03-16 15:09:29 +0200

    Update stackdemo.cpp

--- stackdemo.cpp
+++ stackdemo.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 
+
 using namespace std;
 
 template <typename T>
-class stack {
-public:
-stack (int size) {
-arr = new T[size];
-}
-stack (const stack &s) {
-arr = new T[s.size()];
-arr = s;
-}
-~stack ();
+class stack{
+  public:
+    stack (int size): top(-1), arr(new T [size]) {}
+    stack (const stack &s): top(s.top), arr(new T [sizeof(s.arr)/sizeof(T)]){
+      for (int i=0; i<=top; i++) arr[i]=s.arr[i];
+    }
+    ~stack (){
+      delete [] arr;
+    }
+    const stack & operator = (const stack &s){
+      for (int i=0; i<=s.top; i++) arr[i]=s.arr[i];
+      top=s.top;
+      return *this;
+    }
 
-bool empty () { return top==0; }
-void push (const T &x) { arr[top++]=x; }
-T pop () { return arr[--top]; }
-int size () { 
-return (sizeof(arr)/sizeof(arr[0]));
-}
+    bool empty(){
+      return top==-1;
+    }
+    void push (const T &x){
+      arr[++top]=x;
+    }
+    T pop(){
+      T x=arr[top];
+      top--;
+      return x;
+    }
+    int size(){
+      return top+1;
+    }
 
-friend ostream & operator << (ostream &out, const stack &s) {
-cout<<"[";
-for(int i=0;i<s.size()-1;i++){
-cout<<arr[0]<<", ";
-}
-cout<<arr[i]<<"]"<<endl;
-}
+    friend ostream & operator << (ostream &out, const stack &s){
+      out<<"[";
+      for (int i=0; i<s.top; i++) out<<s.arr[i]<<", ";
+      if (s.top>=0) out<<s.arr[s.top];
+      out<<"]";
+      return out;
+    }
 
-private:
-int top=0;
+  private:
+    int top;
+    T *arr;
 };
+
